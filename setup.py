e4ee1b3ec9b77b1dab161945407b3998460600cb
Lukas Waymann <meribold@gmail.com>
2018-10-26 18:59:48 +0800

    Add `setup.py`

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open('README.md') as file_object:
+    long_description = file_object.read()
+
+setuptools.setup(
+    name='timethis',
+    version='0.0.1',
+    author='Lukas Waymann',
+    author_email='meribold@gmail.com',
+    description='Context manager for measuring execution times',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/meribold/timethis',
+    py_modules=['timethis'],
+    python_requires='~=3.6',
+)
