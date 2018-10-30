e152c227fe53f81dbeed6c1be6b189816f772eec
Lukas Waymann <meribold@gmail.com>
2018-10-30 12:19:42 +0800

    Use thread-local time stacks
    This prevents prefixing messages with too many `│` characters when
    `timethis` is used from multiple threads at the same time.

--- timethis.py
+++ timethis.py
@@ -1,18 +1,21 @@
 from contextlib import contextmanager
+import threading
 from time import perf_counter as _timer
 
-_time_stack = []
+_tld = threading.local()
 
 
 def _push_time():
-    _time_stack.append(_timer())
+    if not hasattr(_tld, 'time_stack'):
+        _tld.time_stack = []
+    _tld.time_stack.append(_timer())
 
 
 def _pop_time(message=None, log=print):
-    elapsed_time = _timer() - _time_stack.pop()
+    elapsed_time = _timer() - _tld.time_stack.pop()
     if message is None:
         return
-    depth = len(_time_stack)
+    depth = len(_tld.time_stack)
     log('│ ' * depth + f'{message}: {elapsed_time:.3f} seconds')
 
 
