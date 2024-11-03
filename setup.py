be4e5ca89e809149523d6e4dc3fcb1dfaa116f44
Lukas Waymann <git@meribold.org>
2024-11-03 16:49:07 +0100

    Call this version 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as file_object:
 
 setuptools.setup(
     name='timethis',
-    version='0.1.1',
+    version='0.1.2',
     author='Lukas Waymann',
     author_email='meribold@gmail.com',
     description='Context manager for measuring execution times',
